77850493a1f3e57ee487adea0350d777476be222
Pär Svedberg <par.svedberg@gmail.com>
2016-10-19 12:21:27 +0200

    Updated component scan as alias in @SpringBootApplication annotation

--- basic-rest/src/main/java/se/kits/svedberg/application/Application.java
+++ basic-rest/src/main/java/se/kits/svedberg/application/Application.java
@@ -9,9 +9,9 @@ import se.kits.svedberg.service.ExperienceServiceBean;
 /**
  * Created by Pär Svedberg on 2016-10-18.
  */
-@SpringBootApplication
-@ComponentScan(basePackageClasses = ExperienceController.class)
-@ComponentScan(basePackageClasses = ExperienceServiceBean.class)
+@SpringBootApplication(scanBasePackages={"se.kits.svedberg.service", "se.kits.svedberg.controller"})
+//@ComponentScan(basePackageClasses = ExperienceController.class)
+//@ComponentScan(basePackageClasses = ExperienceServiceBean.class)
 public class Application {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
